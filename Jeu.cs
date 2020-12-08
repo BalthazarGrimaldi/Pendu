9aec45b0eddec110f724501903b000a28d47ba5e
BalthazarGrimaldi <balthazar.grimaldi@gmail.com>
2020-12-08 18:33:10 +0100

    Réorganisation du code, la fonction de déroulement du jeu est maintenant dans Jeu.cs

--- Jeu.cs
+++ Jeu.cs
@@ -1,30 +1,49 @@
-/*using System;
+using System;
 
 namespace Pendu
 {
     class Jeu
     {
-        private String nom;
-        private int point;
-
-        public String Nom
+        public Jeu() { }
+        public void play()
         {
-            get { return nom; }
-            set { nom = value; }
-        }
-
-        public int Point
-        {
-            get { return point; }
-            set { point = value; }
-        }
-
-
-        public Joueur(String nom, int point)
-        {
-            this.nom = nom;
-            this.point = point;
+            Mot leMot = new Mot("saperlipopette");
+            Grille myGrid = new Grille();
+            String value = "";
+            char i;
+            int pointMalus = 0;
+            String difficulty = "";
+            while (difficulty != "facile" && difficulty != "moyen" && difficulty != "difficile")
+            {
+                Console.WriteLine("Choisisez l'un des trois niveaux de difficultés (facile/moyen/difficile)");
+                difficulty = Console.ReadLine();
+            }
+            Console.WriteLine(leMot.MotTrou);
+            while (!myGrid.isLoose() && !leMot.isWin())
+            {
+                while (value.Length != 1)
+                {
+                    Console.WriteLine("Donnez une lettre !");
+                    value = Console.ReadLine();
+                }
+                i = System.Convert.ToChar(value);
+                value = "";
+                if (!leMot.trouverLettre(i))
+                {
+                    pointMalus = myGrid.difficultyToPoint(difficulty, pointMalus);
+                }
+                myGrid.modifyGrid(pointMalus);
+                myGrid.displayGrid();
+                Console.WriteLine(leMot.MotTrou);
+            }
+            if (myGrid.isLoose())
+            {
+                Console.WriteLine("Vous avez perdus !!");
+            }
+            else
+            {
+                Console.WriteLine("Vous avez gagné !!");
+            }
         }
     }
 }
-*/
--- Program.cs
+++ Program.cs
@@ -6,55 +6,13 @@ namespace Pendu
     {
         static void Main(string[] args)
         {
-            Mot leMot = new Mot("saperlipopette");
-            Grille myGrid = new Grille();
-            String value = "";
-            char i;
-            int pointMalus = 0;
-            String difficulty = "";
-            while (difficulty != "facile" && difficulty != "moyen" && difficulty != "difficile")
-            {
-                Console.WriteLine("Choisisez l'un des trois niveaux de difficultés (facile/moyen/difficile)");
-                difficulty = Console.ReadLine();
-            }
-            Console.WriteLine(leMot.MotTrou);
-            while (!myGrid.isLoose() && !leMot.isWin())
-            {
-                while (value.Length != 1)
-                {
-                    Console.WriteLine("Donnez une lettre !");
-                    value = Console.ReadLine();
-                }
-                i = System.Convert.ToChar(value);
-                value = "";
-                if (!leMot.trouverLettre(i))
-                {
-                    pointMalus = myGrid.difficultyToPoint(difficulty, pointMalus);
-                }
-                myGrid.modifyGrid(pointMalus);
-                myGrid.displayGrid();
-                Console.WriteLine(leMot.MotTrou);
-            }
-            if (myGrid.isLoose())
-            {
-                Console.WriteLine("Vous avez perdus !!");
-            }
-            else
-            {
-                Console.WriteLine("Vous avez gagné !!");
-            }
+            Jeu monJeu = new Jeu();
+            monJeu.play();
         }
     }
 }
 
 /*
-
- String a = "Joseph Staline";
-            char[] x = a.ToCharArray(0, a.Length);
-            foreach (var sub in x)
-            {
-                Console.WriteLine($"Substring: {sub}");
-            }
    ______
   |     |
   |     O
